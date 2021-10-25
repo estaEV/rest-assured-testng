b487aaef4cfb610d410d176aa9930a9c71207f6e
DESKTOP-BSK5N4E\Lenovo <evgeni.drazhev@estafet.com>
2021-10-26 01:24:19 +0300

    maidStuff

--- src/test/java/stepdefs/StepDefinitionRestAssured.java
+++ src/test/java/stepdefs/StepDefinitionRestAssured.java
@@ -3,14 +3,7 @@ package stepdefs;
 import io.cucumber.datatable.DataTable;
 import io.cucumber.java.en.Given;
 import io.restassured.RestAssured;
-import io.restassured.builder.RequestSpecBuilder;
-import io.restassured.builder.ResponseSpecBuilder;
 import io.restassured.response.ValidatableResponse;
-import io.restassured.specification.RequestSpecification;
-import io.restassured.specification.ResponseSpecification;
-import org.apache.http.HttpStatus;
-import org.testng.annotations.BeforeSuite;
-import utils.ConfigFactory;
 import utils.EmployeeAlreadyCreatedException;
 import utils.GlobalVar;
 import utils.NonExistingEmployeeException;
@@ -42,19 +35,19 @@ public class StepDefinitionRestAssured implements GlobalVar {
         try {
             int currentUserId = RestAssured
                     .given()
-                    .spec(getManualRequestSpec())
-//                    .body("{\"name\":Eugene,\"salary\":\"someRand\",\"age\":\"26\"}")
-                    .body(body.toString())
+                        .spec(getManualRequestSpec())
+//                      .body("{\"name\":Eugene,\"salary\":\"someRand\",\"age\":\"26\"}")
+                        .body(body.toString())
                     .when()
-                    .post(GlobalVar.POST_URL)
+                        .post(GlobalVar.POST_URL)
                     .then()
-                    .spec(getManualResponseSpec())
-                    .body("status", equalTo("success"))
-                    .body("data.id", notNullValue())
-                    .body("message", equalTo("Successfully! Record has been added."))
-                    .log().body()
-                    .extract()
-                    .path("data.id");
+                        .spec(getManualResponseSpec())
+                        .body("status", equalTo("success"))
+                        .body("data.id", notNullValue())
+                        .body("message", equalTo("Successfully! Record has been added."))
+                        .log().body()
+                        .extract()
+                        .path("data.id");
 
             if (createdEmployeesIds.contains(currentUserId))
                 throw new EmployeeAlreadyCreatedException("That string is coming from utils.EmployeeAlreadyCreatedException \"throw new\" inside the try block.");
@@ -72,15 +65,15 @@ public class StepDefinitionRestAssured implements GlobalVar {
     public void deleteUser(int arg0) {
         RestAssured
                 .given()
-                .spec(getManualRequestSpec())
+                    .spec(getManualRequestSpec())
                 .when()
-                .delete(GlobalVar.DELETE_URL + arg0)
+                    .delete(GlobalVar.DELETE_URL + arg0)
                 .then()
-                .spec(getManualResponseSpec())
-                .body("status", equalTo("success"))
-                .body("data", equalTo(String.valueOf(arg0)))
-                .body("message", equalTo("Successfully! Record has been deleted"))
-                .log().body();
+                   .spec(getManualResponseSpec())
+                    .body("status", equalTo("success"))
+                    .body("data", equalTo(String.valueOf(arg0)))
+                    .body("message", equalTo("Successfully! Record has been deleted"))
+                    .log().body();
     }
 
     @Given("Get employee with ID {int} request is made")
@@ -89,15 +82,15 @@ public class StepDefinitionRestAssured implements GlobalVar {
         try {
             ValidatableResponse response = RestAssured
                     .given()
-                    .spec(getManualRequestSpec())
+                        .spec(getManualRequestSpec())
                     .when()
-                    .get(GlobalVar.GET_URL + String.valueOf(arg0))
+                        .get(GlobalVar.GET_URL + String.valueOf(arg0))
                     .then()
-                    .spec(getManualResponseSpec())
-                    .body("status", equalTo("success"))
-                    .body("data.id", equalTo(arg0))
-                    .body("message", equalTo("Successfully! Record has been fetched."))
-                    .log().body();
+                        .spec(getManualResponseSpec())
+                        .body("status", equalTo("success"))
+                        .body("data.id", equalTo(arg0))
+                        .body("message", equalTo("Successfully! Record has been fetched."))
+                        .log().body();
 
             if (response.extract().statusCode() >= 300) {
                 throw new NonExistingEmployeeException("That string is coming from utils.NonExistingEmployeeException \"throw new\" inside the try block.");
@@ -112,15 +105,15 @@ public class StepDefinitionRestAssured implements GlobalVar {
     public void getAllUsers() {
         ValidatableResponse response = RestAssured
                 .given()
-                .spec(getManualRequestSpec())
+                    .spec(getManualRequestSpec())
                 .when()
-                .get(GlobalVar.GET_ALL_URL)
+                    .get(GlobalVar.GET_ALL_URL)
                 .then()
-                .spec(getManualResponseSpec())
-                .body("status", equalTo("success"))
-                .body("message", equalTo("Successfully! All records has been fetched."))
-                .body("data.employee_name", hasItems("Doris Wilder", "Bradley Greer", "Tatyana Fitzpatrick"))
-                .log().body();
+                    .spec(getManualResponseSpec())
+                    .body("status", equalTo("success"))
+                    .body("message", equalTo("Successfully! All records has been fetched."))
+                    .body("data.employee_name", hasItems("Doris Wilder", "Bradley Greer", "Tatyana Fitzpatrick"))
+                    .log().body();
     }
 
 
